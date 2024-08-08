bdd19b9916b738fdc1c4d7cb8ad2331c0323d4d5
Augusto Sousa <lagrange.tesla@gmail.com>
2024-08-08 13:37:44 -0600

    Add expenses view

---
+++ resources/js/Pages/Expenses.vue
@@ -0,0 +1,27 @@
+<script setup>
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
+import { Head } from "@inertiajs/vue3";
+</script>
+
+<template>
+    <Head title="Test" />
+
+    <AuthenticatedLayout>
+        <template #header>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                Add a new expense
+            </h2>
+        </template>
+        <div>
+            <div class="py-12">
+                <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                    <div
+                        class="bg-white overflow-hidden shadow-sm sm:rounded-lg"
+                    >
+                        <h1>Salu2</h1>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </AuthenticatedLayout>
+</template>

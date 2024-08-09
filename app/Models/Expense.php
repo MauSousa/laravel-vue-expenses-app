8effd63b4ee6166ebdb07b290382a154e92a24bc
Augusto Sousa <lagrange.tesla@gmail.com>
2024-08-08 23:29:31 -0600

    Add eloquent relationships

--- app/Models/Expense.php
+++ app/Models/Expense.php
@@ -4,8 +4,31 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Expense extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'payment_id',
+        'message',
+        'store_id',
+        'amount',
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function payment(): BelongsTo
+    {
+        return $this->belongsTo(Payment::class);
+    }
+
+    public function store(): BelongsTo
+    {
+        return $this->belongsTo(Store::class);
+    }
 }
--- app/Models/Store.php
+++ app/Models/Store.php
@@ -4,8 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Store extends Model
 {
     use HasFactory;
+
+    public function expense(): HasMany
+    {
+        return $this->hasMany(Expense::class);
+    }
 }

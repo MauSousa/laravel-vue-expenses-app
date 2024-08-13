568ce9de61d248c6b74f24a782382aa892ce7334
Augusto Sousa <lagrange.tesla@gmail.com>
2024-08-13 01:10:27 -0600

    Add link to download monthly expenses pdf

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -4,6 +4,8 @@ import NavLink from "@/Components/NavLink.vue";
 import { Head, useForm } from "@inertiajs/vue3";
 import ItemCard from "@/Components/ItemCard.vue";
 import Pagination from "@/Components/Pagination.vue";
+import { computed } from "vue";
+import { usePage } from "@inertiajs/vue3";
 
 const props = defineProps(["expenses", "date"]);
 
@@ -12,6 +14,12 @@ const date = props.date;
 const form = useForm({
     date: date,
 });
+
+const month = date.split("-")[1];
+
+const page = usePage();
+
+const user = computed(() => page.props.auth.user);
 </script>
 
 <template>
@@ -35,7 +43,29 @@ const form = useForm({
                     <input type="date" class="rounded-lg border border-transparent" v-model="form.date" />
                 </form>
             </div>
-            <NavLink :href="route('expenses.create')"> Add expense </NavLink>
+            <div class="flex justify-center gap-x-5">
+                <div>
+                    <a :href="'/pdf/' + user.id + '?month=' + month"
+                        class="inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out"
+                        target="_blank">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-file-type-pdf"
+                            width="30" height="30" viewBox="0 0 24 24" stroke-width="1" stroke="#2c3e50" fill="none"
+                            stroke-linecap="round" stroke-linejoin="round">
+                            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                            <path d="M14 3v4a1 1 0 0 0 1 1h4" />
+                            <path d="M5 12v-7a2 2 0 0 1 2 -2h7l5 5v4" />
+                            <path d="M5 18h1.5a1.5 1.5 0 0 0 0 -3h-1.5v6" />
+                            <path d="M17 18h2" />
+                            <path d="M20 15h-3v6" />
+                            <path d="M11 15v6h1a2 2 0 0 0 2 -2v-2a2 2 0 0 0 -2 -2h-1z" />
+                        </svg>
+                        <span class="lg:text-sm">Monthly expenses</span>
+                    </a>
+                </div>
+                <NavLink :href="route('expenses.create')">
+                    Add expense
+                </NavLink>
+            </div>
         </div>
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
